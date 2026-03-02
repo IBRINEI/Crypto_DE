56972f054c8705f3b288f71fa540084b25f2c414
BRINE <rogozhin.pd@phystech.edu>
2026-03-02 14:41:58 +0800

    Added RabbitMQ queue support.

---
+++ producer.py
@@ -0,0 +1,34 @@
+import pika
+import requests
+import datetime
+import psycopg2
+import time
+import json
+
+time.sleep(20)
+
+credentials = pika.PlainCredentials('user', 'password')
+params = pika.ConnectionParameters(host='rabbitmq', port=5672, credentials=credentials)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.queue_declare(queue='crypto_prices', durable=True)
+
+try:
+    while True:
+        req = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+        if req.status_code == 200:
+            msg = json.dumps(req.json())
+            channel.basic_publish(exchange='', routing_key='crypto_prices', body=msg, properties=pika.BasicProperties(delivery_mode=2))
+            print('Sent to RabbitMQ!')
+        else:
+            print('Status code:', req.status_code)
+        time.sleep(60)
+except KeyboardInterrupt:
+    print('Interrupted from keyboard')
+except Exception as e:
+    print(f'Exception: {e}')
+finally:
+    if connection.is_open and not connection.is_closed:
+        connection.close()
+        print('Connection closed')
---
+++ worker.py
@@ -0,0 +1,33 @@
+import pika
+import time
+import json
+import psycopg2
+
+from main import get_db_connection, insert_data_to_table
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+    data = json.loads(body)
+
+    conn = get_db_connection()
+    if conn:
+        insert_data_to_table(conn, data)
+        conn.close()
+
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+time.sleep(30)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', port=5672, credentials=pika.PlainCredentials('user', 'password')))
+channel = connection.channel()
+
+channel.queue_declare(queue='crypto_prices', durable=True)
+
+channel.basic_qos(prefetch_count=1)
+
+channel.basic_consume(queue='crypto_prices', on_message_callback=callback)
+
+print(' [*] Waiting for messages. To exit press Ctrl+C')
+channel.start_consuming()
