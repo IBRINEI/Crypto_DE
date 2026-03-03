dc7bf198713ccc12f73402f4e7911e53d01b2428
BRINE <rogozhin.pd@phystech.edu>
2026-03-03 13:43:51 +0800

    Added SMA line to dashboard.

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,15 @@ st.title("🏭 Мониторинг биткоина")
 
 def load_data():
     conn = get_db_connection()
-    query = 'SELECT * FROM bitcoin_rates ORDER BY created_at DESC'
+    query = '''SELECT 
+    created_at,
+    price,
+    AVG(price) OVER (
+        ORDER BY created_at 
+        ROWS BETWEEN 9 PRECEDING AND CURRENT ROW
+    ) as sma_10
+FROM bitcoin_rates
+ORDER BY created_at DESC'''
     data = pd.read_sql_query(query, conn)
     conn.close()
     return data
@@ -31,13 +39,19 @@ while True:
                 y=alt.Y('price', title='Цена (USD)', scale=alt.Scale(zero=False, padding=1))
             )
 
+            sma = alt.Chart(data).encode(
+                x=alt.X('created_at', title='Время'),
+                y=alt.Y('sma_10', title='Цена (USD)', scale=alt.Scale(zero=False, padding=1))
+            )
+
             line = base.mark_line()
+            line_sma = sma.mark_line(strokeDash=[5, 5], color='red', strokeWidth=0.5)
 
             points = base.mark_circle(size=500, opacity=0).encode(
                 tooltip=['created_at', 'price']
             )
 
-            final_chart = (line + points).interactive()
+            final_chart = (line + points + line_sma).interactive()
 
             st.altair_chart(final_chart, use_container_width=True)
 
